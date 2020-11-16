d005bf53e19f79200621d28ddc2c968efd0b857f
Brent Cook <busterb@gmail.com>
2020-11-16 02:20:40 -0600

    initialize peercount
    gcc on several platforms complains about this. It might not be a problem
    but it's not clear from the state machine.

--- src/usr.sbin/ntpd/ntp.c
+++ src/usr.sbin/ntpd/ntp.c
@@ -550,7 +550,7 @@ ntp_dispatch_imsg_dns(void)
 	u_int16_t		 dlen;
 	u_char			*p;
 	struct ntp_addr		*h;
-	size_t			 addrcount, peercount;
+	size_t			 addrcount, peercount = 0;
 	int			 n;
 
 	if (imsgbuf_read(ibuf_dns) != 1)
